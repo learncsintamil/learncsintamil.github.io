fe944d697ee8d56d90bf7d80f226354caa8ac77b
Tamizhvendan S <tamizh88@gmail.com>
2021-01-10 17:54:57 +0530

    remove utube link from footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -13,28 +13,7 @@
       <div class="md:flex md:items-center md:justify-between">
         <div class="flex justify-center md:order-2">
           
-          <a
-            href="https://www.youtube.com/learncsintamil"
-            target="_blank"
-            class="text-gray-850 hover:text-indigo-650 ml-8"
-          >
-            <span class="sr-only">Youtube</span>
-            <svg
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="h-6 w-6"
-            >
-              <path
-                d="M22.54 6.42a2.78 2.78 0 0 0-1.94-2C18.88 4 12 4 12 4s-6.88 0-8.6.46a2.78 2.78 0 0 0-1.94 2A29 29 0 0 0 1 11.75a29 29 0 0 0 .46 5.33A2.78 2.78 0 0 0 3.4 19c1.72.46 8.6.46 8.6.46s6.88 0 8.6-.46a2.78 2.78 0 0 0 1.94-2 29 29 0 0 0 .46-5.25 29 29 0 0 0-.46-5.33z"
-              />
-              <polygon points="9.75 15.02 15.5 11.75 9.75 8.48 9.75 15.02" />
-            </svg>
-          </a>
-          <LinkedIn class="ml-8" url="https://www.linkedin.com/company/learncsintamil"></LinkedIn>
+          <LinkedIn url="https://www.linkedin.com/company/learncsintamil"></LinkedIn>
           <FacebookIcon url="https://www.facebook.com/learncsintamil"></FacebookIcon>
           <a
             href="https://www.instagram.com/learncsintamil"
